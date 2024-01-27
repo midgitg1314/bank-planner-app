25bcb8e77645755b196f53389c14bcead5390c82
Gage Smith <114100318+midgitg1314@users.noreply.github.com>
2024-01-27 01:30:29 -0700

    Update README.MD.cpp

--- README.MD/README.MD.cpp
+++ README.MD/README.MD.cpp
@@ -1,6 +1,8 @@
 # Overview
 
-I wrote a basic C++ program that displays Hello world to the terminal
+In this program I designed a prgram that would allow users to create bank accounts, add the names of the account and the ballances.
+This program allowed users to store data and retrieve it when it was called on. This program used lists, strings, and classes to complete the necessary prgramming. 
+
 [Software Demo Video](https://www.youtube.com/watch?v=iG9reucjh24&t=3s)
 
 # Development Environment
