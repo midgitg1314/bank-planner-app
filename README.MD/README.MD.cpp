4aaff8d9ae438546d8cda182cd916aef1521be5c
Gage Smith <114100318+midgitg1314@users.noreply.github.com>
2024-01-27 01:25:37 -0700

    Update README.MD.cpp

--- README.MD/README.MD.cpp
+++ README.MD/README.MD.cpp
@@ -1,23 +1,13 @@
 # Overview
 
-{ Important!Do not say in this section that this is college assignment.Talk about what you are trying to accomplish as a software engineer to further your learning. }
-
-{Provide a description of your software}
-
-{Describe your purpose for creating this software.}
-
-{Provide a link to your YouTube demonstration.It should be a one minute demo of the software running and a walkthrough of the code.}
-
-[Software Demo Video](http://youtube.link.goes.here)
+I wrote a basic C++ program that displays Hello world to the terminal
+[Software Demo Video](https://www.youtube.com/watch?v=iG9reucjh24&t=3s)
 
 # Development Environment
 
-{ Describe the tools that you used to develop the software }
-
-{Describe the programming language that you used}
+C++
 
 # Useful Websites
 
-{ Make a list of websites that you found helpful in this project }
-*[Web Site Name](http://url.link.goes.here)
-*[Web Site Name](http ://url.link.goes.here)
+*[W3 Schools](https://www.w3schools.com/cpp/)
+*[Reference Guide for C++](https://cplusplus.com/reference/stl/)
